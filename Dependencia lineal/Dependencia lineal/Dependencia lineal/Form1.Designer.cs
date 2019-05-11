2f6d00cf0653d90e13c649a24e397ab5724e9fff
Octavio Salasar Ramírez <3AMSRO@GMAIL,COM>
2019-05-10 21:56:22 -0700

    Independencia primeros pasos

---
+++ Dependencia lineal/Dependencia lineal/Dependencia lineal/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Dependencia_lineal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxV11 = new System.Windows.Forms.TextBox();
+            this.textBoxV12 = new System.Windows.Forms.TextBox();
+            this.textBoxV13 = new System.Windows.Forms.TextBox();
+            this.textBoxV23 = new System.Windows.Forms.TextBox();
+            this.textBoxV22 = new System.Windows.Forms.TextBox();
+            this.textBoxV21 = new System.Windows.Forms.TextBox();
+            this.textBoxV33 = new System.Windows.Forms.TextBox();
+            this.textBoxV32 = new System.Windows.Forms.TextBox();
+            this.textBoxV31 = new System.Windows.Forms.TextBox();
+            this.buttonDependiente = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxV11
+            // 
+            this.textBoxV11.Location = new System.Drawing.Point(68, 37);
+            this.textBoxV11.Name = "textBoxV11";
+            this.textBoxV11.Size = new System.Drawing.Size(33, 20);
+            this.textBoxV11.TabIndex = 0;
+            // 
+            // textBoxV12
+            // 
+            this.textBoxV12.Location = new System.Drawing.Point(68, 63);
+            this.textBoxV12.Name = "textBoxV12";
+            this.textBoxV12.Size = new System.Drawing.Size(33, 20);
+            this.textBoxV12.TabIndex = 1;
+            // 
+            // textBoxV13
+            // 
+            this.textBoxV13.Location = new System.Drawing.Point(68, 89);
+            this.textBoxV13.Name = "textBoxV13";
+            this.textBoxV13.Size = new System.Drawing.Size(33, 20);
+            this.textBoxV13.TabIndex = 2;
+            // 
+            // textBoxV23
+            // 
+            this.textBoxV23.Location = new System.Drawing.Point(245, 88);
+            this.textBoxV23.Name = "textBoxV23";
+            this.textBoxV23.Size = new System.Drawing.Size(33, 20);
+            this.textBoxV23.TabIndex = 5;
+            // 
+            // textBoxV22
+            // 
+            this.textBoxV22.Location = new System.Drawing.Point(245, 63);
+            this.textBoxV22.Name = "textBoxV22";
+            this.textBoxV22.Size = new System.Drawing.Size(33, 20);
+            this.textBoxV22.TabIndex = 4;
+            // 
+            // textBoxV21
+            // 
+            this.textBoxV21.Location = new System.Drawing.Point(245, 35);
+            this.textBoxV21.Name = "textBoxV21";
+            this.textBoxV21.Size = new System.Drawing.Size(33, 20);
+            this.textBoxV21.TabIndex = 3;
+            // 
+            // textBoxV33
+            // 
+            this.textBoxV33.Location = new System.Drawing.Point(392, 88);
+            this.textBoxV33.Name = "textBoxV33";
+            this.textBoxV33.Size = new System.Drawing.Size(33, 20);
+            this.textBoxV33.TabIndex = 8;
+            // 
+            // textBoxV32
+            // 
+            this.textBoxV32.Location = new System.Drawing.Point(392, 62);
+            this.textBoxV32.Name = "textBoxV32";
+            this.textBoxV32.Size = new System.Drawing.Size(33, 20);
+            this.textBoxV32.TabIndex = 7;
+            // 
+            // textBoxV31
+            // 
+            this.textBoxV31.Location = new System.Drawing.Point(392, 36);
+            this.textBoxV31.Name = "textBoxV31";
+            this.textBoxV31.Size = new System.Drawing.Size(33, 20);
+            this.textBoxV31.TabIndex = 6;
+            // 
+            // buttonDependiente
+            // 
+            this.buttonDependiente.Location = new System.Drawing.Point(266, 184);
+            this.buttonDependiente.Name = "buttonDependiente";
+            this.buttonDependiente.Size = new System.Drawing.Size(133, 40);
+            this.buttonDependiente.TabIndex = 9;
+            this.buttonDependiente.Text = "¿Es dependiente?";
+            this.buttonDependiente.UseVisualStyleBackColor = true;
+            this.buttonDependiente.Click += new System.EventHandler(this.buttonDependiente_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 73);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "(";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(284, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 73);
+            this.label2.TabIndex = 11;
+            this.label2.Text = ")";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(333, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 73);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "(";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(186, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 73);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "(";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(623, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 73);
+            this.label5.TabIndex = 14;
+            this.label5.Text = ")";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(431, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 73);
+            this.label6.TabIndex = 15;
+            this.label6.Text = ")";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(107, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 73);
+            this.label7.TabIndex = 16;
+            this.label7.Text = ")";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(537, 37);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 73);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "(";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(596, 37);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 24);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(596, 63);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 24);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(596, 89);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(21, 24);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(490, 63);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(22, 24);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 302);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDependiente);
+            this.Controls.Add(this.textBoxV33);
+            this.Controls.Add(this.textBoxV32);
+            this.Controls.Add(this.textBoxV31);
+            this.Controls.Add(this.textBoxV23);
+            this.Controls.Add(this.textBoxV22);
+            this.Controls.Add(this.textBoxV21);
+            this.Controls.Add(this.textBoxV13);
+            this.Controls.Add(this.textBoxV12);
+            this.Controls.Add(this.textBoxV11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxV11;
+        private System.Windows.Forms.TextBox textBoxV12;
+        private System.Windows.Forms.TextBox textBoxV13;
+        private System.Windows.Forms.TextBox textBoxV23;
+        private System.Windows.Forms.TextBox textBoxV22;
+        private System.Windows.Forms.TextBox textBoxV21;
+        private System.Windows.Forms.TextBox textBoxV33;
+        private System.Windows.Forms.TextBox textBoxV32;
+        private System.Windows.Forms.TextBox textBoxV31;
+        private System.Windows.Forms.Button buttonDependiente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Dependencia lineal/Dependencia lineal/Dependencia lineal/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dependencia_lineal
+{
+    public partial class Form1 : Form
+    {
+        List<TextBox> textBoxList;
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxList = new List<TextBox>()
+            {
+                textBoxV11,textBoxV12,textBoxV13,textBoxV21,textBoxV22,textBoxV23,textBoxV31,textBoxV32,textBoxV33
+            };
+        }
+
+        private void buttonDependiente_Click(object sender, EventArgs e)
+        {
+            if (textBoxList.All(x => x.Text != ""))
+            {
+                if (Determinante(GetMatrix(textBoxList)) == 0)
+                {
+                    MessageBox.Show("El conjunto es linealmente dependiente");
+                }
+                else
+                {
+                    MessageBox.Show("El conjunto es linealmente independiente");
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Rellene todos los campos por favor");
+            }
+        }
+        static double Determinante(double[,] matriz)
+        {
+            double renglon1Positivo = matriz[0, 0] * matriz[1, 1] * matriz[2, 2];
+            double renglon2Positivo = matriz[0, 1] * matriz[1, 2] * matriz[2, 0];
+            double renglon3Positivo = matriz[0, 2] * matriz[1, 0] * matriz[2, 1];
+            double renglon1Negativo = matriz[2, 0] * matriz[1, 1] * matriz[0, 2];
+            double renglon2Negativo = matriz[2, 1] * matriz[1, 2] * matriz[0, 0];
+            double renglon3Negativo = matriz[2, 2] * matriz[1, 0] * matriz[0, 1];
+
+            return (renglon1Positivo + renglon2Positivo + renglon3Positivo - renglon1Negativo - renglon2Negativo - renglon3Negativo);
+        }
+
+        private double[,] GetMatrix(List<TextBox> list)
+        {
+            int index = 0;
+            double[,] temp = new double[3,3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    temp[i, j] = Convert.ToDouble(list[index].Text);
+                    index++;
+                }
+            }
+            return temp;
+        }
+    }
+}
